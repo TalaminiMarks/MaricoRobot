88b3e545df87211aaed874865ecc12e023a1f5ba
TalaminiMarks <talamini2014@gmail.com>
2024-10-02 15:43:16 -0300

    unstall nodemon and formate files

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ import { Client, Events, GatewayIntentBits } from 'discord.js'
 
 const app = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-app.once(Events.ClientReady, clientReady =>{
-  console.log("Ready! Logged as "+ clientReady.user.tag)
+app.once(Events.ClientReady, clientReady => {
+    console.log('Ready! Logged as ' + clientReady.user.tag)
 })
 
 app.login(process.env.DISCORD_TOKEN)
