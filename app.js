afe1e49dfe6a14db48aa487e28dce7db539573b1
TalaminiMarks <talamini2014@gmail.com>
2024-10-04 09:33:53 -0300

    handle slash commands

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-import 'dotenv/config';
+const dotenv = require('dotenv');
+dotenv.config();
 
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const app = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-app.once(Events.ClientReady, clientReady => {
-	console.log('Ready! Logged as ' + clientReady.user.tag);
-});
+app.commands = new Collection();
 
-app.login(process.env.DISCORD_TOKEN);
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			app.commands.set(command.data.name, command);
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+
+	}
+}
+
+// app.once(Events.ClientReady, clientReady => {
+// 	console.log('Ready! Logged as ' + clientReady.user.tag);
+// });
+
+// app.login(process.env.DISCORD_TOKEN);
