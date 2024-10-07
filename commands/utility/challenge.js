4f83f69e9ade64efcd8b28f5e3b05942e2e618bd
TalaminiMarks <talamini2014@gmail.com>
2024-10-06 21:17:54 -0300

    testing input command with choise

--- commands/utility/challenge.js
+++ commands/utility/challenge.js
@@ -1,16 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { scheduler } = require('node:timers/promises');
+// const { scheduler } = require('node:timers/promises');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('pedra-papel-tesoura')
-		.setDescription('Joga Pedra Papel Tesoura com o bot'),
+		.setDescription('Joga Pedra Papel Tesoura com o bot')
+		.addStringOption(option =>
+			option.setName('input')
+				.setDescription('test string option')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Pedra', value: '1' },
+				),
+		),
 	async execute(interaction) {
-	    await interaction.deferReply();
-		await scheduler.wait(1000);
-		await interaction.followUp('Pong Again!');
-		await scheduler.wait(1000);
-		const teste = await interaction.fetchReply();
-		console.log(teste.interaction);
+	    await interaction.reply('teste');
+		console.log(interaction);
 	},
 };
