5506f6d3bee405064cac9f0ee63661167cb8195d
TalaminiMarks <talamini2014@gmail.com>
2024-10-07 19:52:23 -0300

    remove interface import

--- commands/utility/challenge.js
+++ commands/utility/challenge.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const { scheduler } = require('node:timers/promises');
 
 module.exports = {
