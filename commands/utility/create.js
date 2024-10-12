9c47540f22f91ee03521607ca9d2eec2b75c1dfd
TalaminiMarks <talamini2014@gmail.com>
2024-10-12 13:45:29 -0300

    add 'criar' command. Create a channel for creating rpg channel

--- commands/utility/create.js
+++ commands/utility/create.js
@@ -1,13 +1,60 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('criar')
-		.setDescription('comando para criar um personagem de RPG'),
-
+		.setDescription('comando para criar um personagem de RPG')
+		.addStringOption(option =>
+			option
+				.setName('nome')
+				.setDescription('Nome do personagem')
+				.setRequired(true)
+				.setMinLength(1)
+				.setMaxLength(99),
+		),
 	async execute(interaction) {
-		console.log(interaction.client);
+		await interaction.reply({ content: 'Criando canal...', fetchReply: true });
 
-		interaction.reply('teste');
+		const charName = interaction.options.getString('nome');
+		let categoryId = undefined;
+
+		try {
+			await interaction.guild.channels.fetch()
+				.then(channels => {
+					categoryId = channels.findKey(channel => channel.name === '*Criando Personagem*');
+				});
+
+			if (!categoryId) {
+				const category = await interaction.guild.channels.create({
+					name: '*Criando Personagem*',
+					type: ChannelType.GuildCategory,
+				});
+
+				await interaction.guild.channels.create({
+					name: charName,
+					type: ChannelType.GuildText,
+				})
+					.then(channel => {
+						channel.setParent(category.id);
+					});
+			}
+
+			else {
+				await interaction.guild.channels.create({
+					name: charName,
+					type: ChannelType.GuildText,
+				})
+					.then(channel => {
+						channel.setParent(categoryId);
+					});
+			}
+
+			await interaction.editReply({ content:'Canal Criado!' });
+		}
+
+		catch (error) {
+			console.log(error);
+			await interaction.editReply({ content: 'Erro ao criar o canal' });
+		}
 	},
 };
