8ab0fea1cd3e0676864c30bb9e6e94f47d8ad893
TalaminiMarks <talamini2014@gmail.com>
2024-10-16 14:31:31 -0300

    setup basic version of '/criar-personagem' command

--- commands/utility/create-character.js
+++ commands/utility/create-character.js
@@ -1,8 +1,9 @@
 // eslint-disable-next-line no-unused-vars
-const { Interaction, SlashCommandBuilder, TextInputBuilder, ActionRowBuilder } = require('discord.js');
+const { Interaction, SlashCommandBuilder } = require('discord.js');
 const { getRandomEmoji } = require('../../utils');
 
 module.exports = {
+	// Info basica do comando
 	data: new SlashCommandBuilder()
 		.setName('criar-personagem')
 		.setDescription('Comando para criar o personagem no canal especifico usando o "/criar"'),
@@ -10,20 +11,58 @@ module.exports = {
 	/**
      * @param {Interaction} interaction
      */
+	// Função de execução do comando
 	async execute(interaction) {
+		// Dispara uma interação bem sucedida do discord
 		await interaction.deferReply();
-
+		// Variavel para armazenar se o canal da interação e cargo do usuário tem o mesmo nome
 		let validation = false;
+		// Busca o nos membros do server, pelo id do usuario da interação
+		// Se o cargo do membro tem o mesmo nome do canal de interação
 		await interaction.guild.members.fetch(interaction.user.id)
 			.then(member => {
 				validation = member.roles.cache.some(role => role.name.toLowerCase() === interaction.channel.name);
 			});
-
+		// Se o nome for diferente, vai dispara uma mensagem com as orientações para usuário
 		if (!validation) {
 			await interaction.editReply('Você precisa executar o comando /criar com o nome do personagem antes ' + getRandomEmoji());
 		}
+
 		else {
-			await interaction.editReply('passow');
-		}
+			// Define o filtro para as respostas das perguntas
+			const messageFilter = m => m.author.id === interaction.user.id;
+			// Define o Obj com as opções da função awaitMessages
+			const awaitObj = { filter: messageFilter, max: 1, time: 5000, errors: ['time'] };
+
+			try {
+				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
+				await interaction.followUp({ content: 'Qual seu nome?' })
+					.then(async () => {
+						await interaction.channel.awaitMessages(awaitObj)
+							.then(collected => {
+								interaction.followUp(collected.first().content);
+							})
+							.catch(() => {
+								throw new Error('Tempo do comando espirado, execute o comando novamente');
+							});
+					});
+				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
+				await interaction.followUp('Qual sua idade?')
+					.then(async () => {
+						await interaction.channel.awaitMessages(awaitObj)
+							.then(collected => {
+								interaction.followUp(collected.first().content);
+							})
+							.catch(() => {
+								throw new Error('Tempo do comando espirado, execute o comando novamente');
+							});
+					});
+			}
+			// Retorna para o usuário se der algum erro durante a execução do comando
+			catch (error) {
+				interaction.followUp(error.message);
+			}
+		};
+
 	},
 };
