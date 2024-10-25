e2f0db272dc50b950ec29b33129ba0e40e491cea
TalaminiMarks <talamini2014@gmail.com>
2024-10-25 10:12:45 -0300

    add zod schema for parse data to send to db

--- commands/utility/create-character.js
+++ commands/utility/create-character.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 const { Interaction, SlashCommandBuilder } = require('discord.js');
+const z = require('zod');
 const { getRandomEmoji } = require('../../utils');
 
 module.exports = {
@@ -15,14 +16,26 @@ module.exports = {
 	async execute(interaction) {
 		// Dispara uma interação bem sucedida do discord
 		await interaction.deferReply();
+
+		// Objeto para cadastro no banco de dados
+		const characterSchema = {};
+
 		// Variavel para armazenar se o canal da interação e cargo do usuário tem o mesmo nome
 		let validation = false;
+
 		// Busca o nos membros do server, pelo id do usuario da interação
 		// Se o cargo do membro tem o mesmo nome do canal de interação
 		await interaction.guild.members.fetch(interaction.user.id)
 			.then(member => {
-				validation = member.roles.cache.some(role => role.name.toLowerCase() === interaction.channel.name);
+				member.roles.cache.filter(role => {
+					if (role.name.toLowerCase() === interaction.channel.name) {
+						validation = true;
+						characterSchema.id = role.id;
+						characterSchema.name = role.name;
+					}
+				});
 			});
+
 		// Se o nome for diferente, vai dispara uma mensagem com as orientações para usuário
 		if (!validation) {
 			await interaction.editReply('Você precisa executar o comando /criar com o nome do personagem antes ' + getRandomEmoji());
@@ -33,6 +46,11 @@ module.exports = {
 			const messageFilter = m => m.author.id === interaction.user.id;
 			// Define o Obj com as opções da função awaitMessages
 			const awaitObj = { filter: messageFilter, max: 1, time: 5000, errors: ['time'] };
+			// Schema para validação dos dados
+			const schema = z.object({
+				id: z.string(),
+				name: z.string(),
+			});
 
 			try {
 				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
@@ -40,7 +58,7 @@ module.exports = {
 					.then(async () => {
 						await interaction.channel.awaitMessages(awaitObj)
 							.then(collected => {
-								interaction.followUp(collected.first().content);
+								// characterSchema.other = collected.first().content;
 							})
 							.catch(() => {
 								throw new Error('Tempo do comando espirado, execute o comando novamente');
@@ -51,12 +69,14 @@ module.exports = {
 					.then(async () => {
 						await interaction.channel.awaitMessages(awaitObj)
 							.then(collected => {
-								interaction.followUp(collected.first().content);
+								characterSchema.age = Number(collected.first().content);
 							})
 							.catch(() => {
 								throw new Error('Tempo do comando espirado, execute o comando novamente');
 							});
 					});
+				const data = schema.parse(characterSchema);
+				console.log(data);
 			}
 			// Retorna para o usuário se der algum erro durante a execução do comando
 			catch (error) {
