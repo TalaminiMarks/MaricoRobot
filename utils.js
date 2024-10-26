306da4d49d75012cd3f0b81618da7b782cefcbf5
TalaminiMarks <talamini2014@gmail.com>
2024-10-26 15:38:41 -0300

    add axios instance

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 module.exports = {
 	capitalize: (string) => {
 		return string.charAt(0).toUpperCase() + string.slice(1);
@@ -6,7 +8,14 @@ module.exports = {
 		const emojiList = ['😭', '😄', '😌', '🤓', '😎', '😤', '🤖', '😶‍🌫️', '🌏', '📸', '💿', '👋', '🌊', '✨'];
 		return emojiList[Math.floor(Math.random() * emojiList.length)];
 	},
-	async sendMessage(interaction, channelId, string) {
+	sendMessage: async function(interaction, channelId, string) {
 		await interaction.client.channels.cache.get(channelId).send(string);
 	},
+	axios: axios.create({
+		baseURL: 'http://127.0.0.1:3333/',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		timeout: 5000,
+	}),
 };
