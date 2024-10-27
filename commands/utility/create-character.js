c7950723ede364ffbb81458c6156d8d45556a284
TalaminiMarks <talamini2014@gmail.com>
2024-10-27 15:52:27 -0300

    add select menu to message

--- commands/utility/create-character.js
+++ commands/utility/create-character.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-const { Interaction, SlashCommandBuilder } = require('discord.js');
+const { Interaction, SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
 const { getRandomEmoji, axios } = require('../../utils');
 
 module.exports = {
@@ -41,37 +41,40 @@ module.exports = {
 		}
 
 		else {
-			// Define o filtro para as respostas das perguntas
-			const messageFilter = m => m.author.id === interaction.user.id;
-			// Define o Obj com as opções da função awaitMessages
-			const awaitObj = { filter: messageFilter, max: 1, time: 5000, errors: ['time'] };
-
 			try {
+				const select = new StringSelectMenuBuilder()
+					.setPlaceholder('Selecione a sua classe')
+					.setCustomId('class')
+					.addOptions(
+						new StringSelectMenuOptionBuilder().setValue('warrior').setLabel('Guerreiro').setDescription('O Guerreiro combina uma alta força e destreza'),
+						new StringSelectMenuOptionBuilder().setValue('knight').setLabel('Cavaleiro').setDescription('O Cavaleiro começa com maior vitalidade'),
+						new StringSelectMenuOptionBuilder().setValue('wanderer').setLabel('Andarilho').setDescription('O Andarilho é focado em velocidade e evasão'),
+						new StringSelectMenuOptionBuilder().setValue('thief').setLabel('Ladrão').setDescription('Os Ladrões tem alta chance de critico com adaga'),
+						new StringSelectMenuOptionBuilder().setValue('bandit').setLabel('Bandido').setDescription('Os Bandidos são especializados em ataques fisicos fortes'),
+						new StringSelectMenuOptionBuilder().setValue('hunter').setLabel('Caçador').setDescription('Os Caçadores são especialistas em arcos'),
+						new StringSelectMenuOptionBuilder().setValue('sorcerer').setLabel('Feiticeiro').setDescription('Os Feiticeiros são especialistas em magias'),
+						new StringSelectMenuOptionBuilder().setValue('pyromancer').setLabel('Piromante').setDescription('Os Piromantes são especialistas em pirotecnias'),
+						new StringSelectMenuOptionBuilder().setValue('cleric').setLabel('Clérigo').setDescription('Os Clérigos usam os milagres a seu favor'),
+						new StringSelectMenuOptionBuilder().setValue('deprived').setLabel('Depravado').setDescription('Os Depravados não possuem nada ao seu auxilio'),
+					);
+				const row = new ActionRowBuilder().setComponents(select);
 				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
-				await interaction.followUp({ content: 'Qual será a sua classe?' })
-					.then(async () => {
-						await interaction.channel.awaitMessages(awaitObj)
-							.then(collected => {
-								characterSchema.role = collected.first().content;
-							})
-							.catch(() => {
-								throw new Error('Tempo do comando espirado, execute o comando novamente');
-							});
-					});
-				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
-				await interaction.followUp('Qual será seu item inicial?')
-					.then(async () => {
-						await interaction.channel.awaitMessages(awaitObj)
-							.then(collected => {
-								characterSchema.initial = collected.first().content;
-							})
-							.catch(() => {
-								throw new Error('Tempo do comando espirado, execute o comando novamente');
-							});
-					});
+				await interaction.followUp({ content: 'Qual será a sua classe?', components: [row] });
 
-				axios.post('personagem/criar', characterSchema)
-					.then(res => interaction.followUp(res.data.message));
+				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
+				// await interaction.followUp('Qual será seu item inicial?')
+				// 	.then(async () => {
+				// 		await interaction.channel.awaitMessages(awaitObj)
+				// 			.then(collected => {
+				// 				characterSchema.initial = collected.first().content;
+				// 			})
+				// 			.catch(() => {
+				// 				throw new Error('Tempo do comando espirado, execute o comando novamente');
+				// 			});
+				// 	});
+
+				// axios.post('personagem/criar', characterSchema)
+				// 	.then(res => interaction.followUp(res.data.message));
 			}
 			// Retorna para o usuário se der algum erro durante a execução do comando
 			catch (error) {
