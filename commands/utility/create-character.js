df66a40017404cb11c44d0cedb871465b03f7aa7
TalaminiMarks <talamini2014@gmail.com>
2024-10-28 09:54:57 -0300

    add handler to select menu on interaction

--- commands/utility/create-character.js
+++ commands/utility/create-character.js
@@ -46,39 +46,48 @@ module.exports = {
 					.setPlaceholder('Selecione a sua classe')
 					.setCustomId('class')
 					.addOptions(
-						new StringSelectMenuOptionBuilder().setValue('warrior').setLabel('Guerreiro').setDescription('O Guerreiro combina uma alta força e destreza'),
-						new StringSelectMenuOptionBuilder().setValue('knight').setLabel('Cavaleiro').setDescription('O Cavaleiro começa com maior vitalidade'),
-						new StringSelectMenuOptionBuilder().setValue('wanderer').setLabel('Andarilho').setDescription('O Andarilho é focado em velocidade e evasão'),
-						new StringSelectMenuOptionBuilder().setValue('thief').setLabel('Ladrão').setDescription('Os Ladrões tem alta chance de critico com adaga'),
-						new StringSelectMenuOptionBuilder().setValue('bandit').setLabel('Bandido').setDescription('Os Bandidos são especializados em ataques fisicos fortes'),
-						new StringSelectMenuOptionBuilder().setValue('hunter').setLabel('Caçador').setDescription('Os Caçadores são especialistas em arcos'),
-						new StringSelectMenuOptionBuilder().setValue('sorcerer').setLabel('Feiticeiro').setDescription('Os Feiticeiros são especialistas em magias'),
-						new StringSelectMenuOptionBuilder().setValue('pyromancer').setLabel('Piromante').setDescription('Os Piromantes são especialistas em pirotecnias'),
-						new StringSelectMenuOptionBuilder().setValue('cleric').setLabel('Clérigo').setDescription('Os Clérigos usam os milagres a seu favor'),
-						new StringSelectMenuOptionBuilder().setValue('deprived').setLabel('Depravado').setDescription('Os Depravados não possuem nada ao seu auxilio'),
+						new StringSelectMenuOptionBuilder().setValue('guerreiro').setLabel('Guerreiro').setDescription('O Guerreiro combina uma alta força e destreza'),
+						new StringSelectMenuOptionBuilder().setValue('cavaleiro').setLabel('Cavaleiro').setDescription('O Cavaleiro começa com maior vitalidade'),
+						new StringSelectMenuOptionBuilder().setValue('andarilho').setLabel('Andarilho').setDescription('O Andarilho é focado em velocidade e evasão'),
+						new StringSelectMenuOptionBuilder().setValue('ladrao').setLabel('Ladrão').setDescription('Os Ladrões tem alta chance de critico com adaga'),
+						new StringSelectMenuOptionBuilder().setValue('bandido').setLabel('Bandido').setDescription('Os Bandidos são especializados em ataques fisicos fortes'),
+						new StringSelectMenuOptionBuilder().setValue('cacador').setLabel('Caçador').setDescription('Os Caçadores são especialistas em arcos'),
+						new StringSelectMenuOptionBuilder().setValue('feiticeiro').setLabel('Feiticeiro').setDescription('Os Feiticeiros são especialistas em magias'),
+						new StringSelectMenuOptionBuilder().setValue('piromante').setLabel('Piromante').setDescription('Os Piromantes são especialistas em pirotecnias'),
+						new StringSelectMenuOptionBuilder().setValue('clerigo').setLabel('Clérigo').setDescription('Os Clérigos usam os milagres a seu favor'),
+						new StringSelectMenuOptionBuilder().setValue('depravado').setLabel('Depravado').setDescription('Os Depravados não possuem nada ao seu auxilio'),
 					);
 				let row = new ActionRowBuilder().setComponents(select);
 				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
-				await interaction.followUp({ content: 'Qual será a sua classe?', components: [row] });
+				const characterRole = await interaction.followUp({ content: 'Qual será a sua classe?', components: [row] });
+
+				const componentfilter = i => i.user.id === interaction.user.id;
+				const roleResponse = await characterRole.awaitMessageComponent({ filter: componentfilter, time: 10000 });
+				console.log(roleResponse);
+				roleResponse.update({ content: `Você escolheu ${roleResponse.values} como classe`, components: [] });
+
 
 				select = new StringSelectMenuBuilder()
 					.setPlaceholder('Selecione a sua classe')
 					.setCustomId('class')
 					.addOptions(
-						new StringSelectMenuOptionBuilder().setValue('binoculars').setLabel('Binoculos').setDescription('Um Binoculos'),
-						new StringSelectMenuOptionBuilder().setValue('black firebomb').setLabel('Bomba de fogo Negra').setDescription('Uma bomba negra que explode no impacto'),
-						new StringSelectMenuOptionBuilder().setValue('divine blessing').setLabel('Benção Divina').setDescription('Uma água com a benção das divindades'),
-						new StringSelectMenuOptionBuilder().setValue('master key').setLabel('Chave Mestra').setDescription('Um molho de chaves'),
-						new StringSelectMenuOptionBuilder().setValue('old witchs ring').setLabel('Anel antigo da Bruxa').setDescription('Um anel antigo, empoeirado e enferrujado'),
-						new StringSelectMenuOptionBuilder().setValue('pendant').setLabel('Pingente').setDescription('Um pingente com uma figura desenhada'),
-						new StringSelectMenuOptionBuilder().setValue('tiny beings ring').setLabel('Anel Pequenino').setDescription('Um anel pequeno com uma joia vermelha'),
-						new StringSelectMenuOptionBuilder().setValue('twin humanities').setLabel('Humanidade Gemea').setDescription('Duas humanidades interligadas'),
+						new StringSelectMenuOptionBuilder().setValue('binoculos').setLabel('Binoculos').setDescription('Um Binoculos'),
+						new StringSelectMenuOptionBuilder().setValue('bomba de fogo negra').setLabel('Bomba de fogo Negra').setDescription('Uma bomba negra que explode no impacto'),
+						new StringSelectMenuOptionBuilder().setValue('bencao divina').setLabel('Benção Divina').setDescription('Uma água com a benção das divindades'),
+						new StringSelectMenuOptionBuilder().setValue('chave mestra').setLabel('Chave Mestra').setDescription('Um molho de chaves'),
+						new StringSelectMenuOptionBuilder().setValue('anel antigo da bruxa').setLabel('Anel antigo da Bruxa').setDescription('Um anel antigo, empoeirado e enferrujado'),
+						new StringSelectMenuOptionBuilder().setValue('pingente').setLabel('Pingente').setDescription('Um pingente com uma figura desenhada'),
+						new StringSelectMenuOptionBuilder().setValue('anel pequenino').setLabel('Anel Pequenino').setDescription('Um anel pequeno com uma joia vermelha'),
+						new StringSelectMenuOptionBuilder().setValue('humanidade gemea').setLabel('Humanidade Gemea').setDescription('Duas humanidades interligadas'),
 					);
 
 				row = new ActionRowBuilder().setComponents(select);
 
 				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
-				await interaction.followUp({ content: 'Qual será seu item inicial?', components: [row] });
+				const initialItem = await interaction.followUp({ content: 'Qual será seu item inicial?', components: [row] });
+
+				const initialItemResponse = await initialItem.awaitMessageComponent({ filter: componentfilter, time: 10000 });
+				initialItemResponse.update({ content: `Você escolheu ${initialItemResponse.values} como item inicial`, components: [] });
 
 				axios.post('personagem/criar', characterSchema)
 					.then(res => interaction.followUp(res.data.message));
