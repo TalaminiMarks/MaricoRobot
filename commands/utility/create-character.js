edfd17bc34b874723dcfecb14ab2223db1fba283
TalaminiMarks <talamini2014@gmail.com>
2024-10-29 09:54:20 -0300

    add role and initial item on character schema and more comments

--- commands/utility/create-character.js
+++ commands/utility/create-character.js
@@ -42,6 +42,7 @@ module.exports = {
 
 		else {
 			try {
+				// Armazena um select menu e suas opções. São as classes
 				let select = new StringSelectMenuBuilder()
 					.setPlaceholder('Selecione a sua classe')
 					.setCustomId('class')
@@ -57,19 +58,24 @@ module.exports = {
 						new StringSelectMenuOptionBuilder().setValue('clerigo').setLabel('Clérigo').setDescription('Os Clérigos usam os milagres a seu favor'),
 						new StringSelectMenuOptionBuilder().setValue('depravado').setLabel('Depravado').setDescription('Os Depravados não possuem nada ao seu auxilio'),
 					);
+
+				// Cria uma linha com ação e recebe o select menu
 				let row = new ActionRowBuilder().setComponents(select);
+
 				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
 				const characterRole = await interaction.followUp({ content: 'Qual será a sua classe?', components: [row] });
 
+				// Cria um filtro que valida somente a escolha da pessoa que começou a interação
 				const componentfilter = i => i.user.id === interaction.user.id;
+
+				// Espera até o usuário fazer a sua escolha e response com uma mensagem qual classe ele escolheu
 				const roleResponse = await characterRole.awaitMessageComponent({ filter: componentfilter, time: 10000 });
-				console.log(roleResponse);
-				roleResponse.update({ content: `Você escolheu ${roleResponse.values} como classe`, components: [] });
+				await roleResponse.update({ content: `Você escolheu ${roleResponse.values} como classe`, components: [] });
 
-
+				// Reutiliza a variavel das classes para criar um select menu com os itens inicial
 				select = new StringSelectMenuBuilder()
 					.setPlaceholder('Selecione a sua classe')
-					.setCustomId('class')
+					.setCustomId('initial')
 					.addOptions(
 						new StringSelectMenuOptionBuilder().setValue('binoculos').setLabel('Binoculos').setDescription('Um Binoculos'),
 						new StringSelectMenuOptionBuilder().setValue('bomba de fogo negra').setLabel('Bomba de fogo Negra').setDescription('Uma bomba negra que explode no impacto'),
@@ -81,14 +87,22 @@ module.exports = {
 						new StringSelectMenuOptionBuilder().setValue('humanidade gemea').setLabel('Humanidade Gemea').setDescription('Duas humanidades interligadas'),
 					);
 
+				// Reutiliza a varivel da linha de ação e recebe o select dos itens
 				row = new ActionRowBuilder().setComponents(select);
 
-				// Envia uma pergunta e espera até o usuário mandar uma mensagem como resposta
+				// Envia uma pergunta e espera até o usuário selecionar a opção
 				const initialItem = await interaction.followUp({ content: 'Qual será seu item inicial?', components: [row] });
 
+				// Espera até o usuário fazer a sua escolha e response com uma mensagem qual classe ele escolheu
 				const initialItemResponse = await initialItem.awaitMessageComponent({ filter: componentfilter, time: 10000 });
-				initialItemResponse.update({ content: `Você escolheu ${initialItemResponse.values} como item inicial`, components: [] });
+				await initialItemResponse.update({ content: `Você escolheu ${initialItemResponse.values} como item inicial`, components: [] });
 
+				// Atribui os valores recebidos do usuário para o objeto characterSchema
+				characterSchema.role = roleResponse.values[0];
+				characterSchema.initial = initialItemResponse.values[0];
+
+				// Envia o objeto do usuário para criar um registro no banco de dados.
+				// Se bem sucedido, devolve uma mensagem que deu certo
 				axios.post('personagem/criar', characterSchema)
 					.then(res => interaction.followUp(res.data.message));
 			}
