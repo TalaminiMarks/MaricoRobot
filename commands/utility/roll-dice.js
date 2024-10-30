b35781d8cd3da6dae3044cebc159c2b892763e14
TalaminiMarks <talamini2014@gmail.com>
2024-10-30 09:45:06 -0300

    add dice roll command

---
+++ commands/utility/roll-dice.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('rolar-dado')
+		.setDescription('Seleciona um dado para ser rolado')
+		.addStringOption((option) => {
+			option
+				.setRequired(true)
+				.setName('dado')
+				.setDescription('tipo dos dados')
+				.addChoices(
+					{ name: 'D4', value: 'd4' },
+					{ name: 'D6', value: 'd6' },
+					{ name: 'D8', value: 'd8' },
+					{ name: 'D12', value: 'd12' },
+					{ name: 'D20', value: 'd20' },
+					{ name: 'D100', value: 'd100' },
+					{ name: 'tem dado em casa?', value: 'casa' },
+				);
+		}),
+	/**
+     * @param {import('discord.js').Interaction} interaction
+     */
+	async execute(interaction) {
+		interaction.reply('alo');
+	},
+};
