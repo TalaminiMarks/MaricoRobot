c18784b01c23a89fea2193be26cdf8aef4c3e6d5
TalaminiMarks <talamini2014@gmail.com>
2024-11-01 14:57:13 -0300

    finish dice roll command

--- commands/utility/roll-dice.js
+++ commands/utility/roll-dice.js
@@ -4,25 +4,30 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rolar-dado')
 		.setDescription('Seleciona um dado para ser rolado')
-		.addStringOption(option =>
+		.addIntegerOption(option =>
 			option
 				.setName('input')
 				.setDescription('tipo dos dados')
 				.setRequired(true)
 				.addChoices(
-					{ name: 'D4', value: 'd4' },
-					{ name: 'D6', value: 'd6' },
-					{ name: 'D8', value: 'd8' },
-					{ name: 'D12', value: 'd12' },
-					{ name: 'D20', value: 'd20' },
-					{ name: 'D100', value: 'd100' },
-					{ name: 'tem dado em casa?', value: 'casa' },
+					{ name: 'D4', value: 4 },
+					{ name: 'D6', value: 6 },
+					{ name: 'D8', value: 8 },
+					{ name: 'D12', value: 12 },
+					{ name: 'D20', value: 20 },
+					{ name: 'D100', value: 100 },
 				),
 		),
 	/**
      * @param {import('discord.js').Interaction} interaction
      */
 	async execute(interaction) {
-		interaction.reply('alo');
+		function roll(n) {
+			const result = Math.floor(Math.random() * n) + 1;
+			return result;
+		}
+
+		const output = roll(interaction.options.getInteger('input'));
+		await interaction.reply(String(output));
 	},
 };
