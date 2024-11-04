107b95d2bd0d91ba5ec1a5ff9a9f010d9aee792e
TalaminiMarks <talamini2014@gmail.com>
2024-11-04 08:11:39 -0300

    add comments and remove resulta variable on roll function

--- commands/utility/roll-dice.js
+++ commands/utility/roll-dice.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	// Dados do comando com as suas opções de dados
 	data: new SlashCommandBuilder()
 		.setName('rolar-dado')
 		.setDescription('Seleciona um dado para ser rolado')
@@ -21,12 +22,13 @@ module.exports = {
 	/**
      * @param {import('discord.js').Interaction} interaction
      */
+	// Função pra executar no comando
 	async execute(interaction) {
+		// Função pra gerar um número aleatorio com base no valor do dado
 		function roll(n) {
-			const result = Math.floor(Math.random() * n) + 1;
-			return result;
+			return Math.floor(Math.random() * n) + 1;
 		}
-
+		// Retorna pro usuario da interação o número que foi gerado
 		const output = roll(interaction.options.getInteger('input'));
 		await interaction.reply(String(output));
 	},
