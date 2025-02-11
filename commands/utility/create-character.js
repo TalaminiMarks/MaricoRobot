5f7014e98e4444bb388db81628c05d657aca41f6
TalaminiMarks <talamini2014@gmail.com>
2025-02-11 17:11:40 -0300

    add command to create character

--- commands/utility/create-character.js
+++ commands/utility/create-character.js
@@ -1,10 +1,78 @@
-const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, bold } = require('discord.js');
 const { getRandomEmoji, axios } = require('../../utils');
+
+function subRaceSelector(race) {
+	const avalibleRace = {
+		anao: () => {
+			return [
+				new StringSelectMenuOptionBuilder().setValue('anao da montanha').setDescription('raça Anão da Montanha').setLabel('Anão da Montanha'),
+				new StringSelectMenuOptionBuilder().setValue('anao da colina').setDescription('raça Anão da Colina').setLabel('Anão da Colina'),
+			];
+		},
+		elfo: () => {
+			return [
+				new StringSelectMenuOptionBuilder().setValue('alto elfo').setDescription('raça Alto Elfo').setLabel('Alto Elfo'),
+				new StringSelectMenuOptionBuilder().setValue('elfo da floresta').setDescription('raça Elfo da Floresta').setLabel('Elfo da Floresta'),
+				new StringSelectMenuOptionBuilder().setValue('elfo negro').setDescription('raça Elfo Negro').setLabel('Elfo Negro'),
+			];
+		},
+		halfling: () => {
+			return [
+				new StringSelectMenuOptionBuilder().setValue('halfling pes leves').setDescription('raça Halfling Pes Leves').setLabel('Halfling Pes Leves'),
+				new StringSelectMenuOptionBuilder().setValue('halfling robusto').setDescription('raça Halfling Robusto').setLabel('Halfling Robusto'),
+			];
+		},
+		humano: () => {
+			return [
+				new StringSelectMenuOptionBuilder().setValue('calishita').setDescription('Humano Calishita').setLabel('Calishita'),
+				new StringSelectMenuOptionBuilder().setValue('chondathano').setDescription('Humano Chondathano').setLabel('Chondathano'),
+				new StringSelectMenuOptionBuilder().setValue('damarano').setDescription('Humano Damarano').setLabel('Damarano'),
+				new StringSelectMenuOptionBuilder().setValue('illuskano').setDescription('Humano Illuskano').setLabel('Illuskano'),
+				new StringSelectMenuOptionBuilder().setValue('mulano').setDescription('Humano Mulano').setLabel('Mulano'),
+				new StringSelectMenuOptionBuilder().setValue('rashemita').setDescription('Humano Rashemita').setLabel('Rashemita'),
+				new StringSelectMenuOptionBuilder().setValue('shou').setDescription('Humano Shou').setLabel('Shou'),
+				new StringSelectMenuOptionBuilder().setValue('tethyriano').setDescription('Humano Tethyriano').setLabel('Tethyriano'),
+				new StringSelectMenuOptionBuilder().setValue('turami').setDescription('Humano Turami').setLabel('Turami'),
+			];
+		},
+		draconato: () => {
+			return [
+				new StringSelectMenuOptionBuilder().setValue('azul').setDescription('Ancestral Dracônico Azul').setLabel('Azul'),
+				new StringSelectMenuOptionBuilder().setValue('branco').setDescription('Ancestral Dracônico Branco').setLabel('Branco'),
+				new StringSelectMenuOptionBuilder().setValue('Bronze').setDescription('Ancestral Dracônico Bronze').setLabel('Bronze'),
+				new StringSelectMenuOptionBuilder().setValue('cobre').setDescription('Ancestral Dracônico Cobre').setLabel('Cobre'),
+				new StringSelectMenuOptionBuilder().setValue('latao').setDescription('Ancestral Dracônico Latao').setLabel('Latao'),
+				new StringSelectMenuOptionBuilder().setValue('negro').setDescription('Ancestral Dracônico Negro').setLabel('Negro'),
+				new StringSelectMenuOptionBuilder().setValue('ouro').setDescription('Ancestral Dracônico Ouro').setLabel('Ouro'),
+				new StringSelectMenuOptionBuilder().setValue('prata').setDescription('Ancestral Dracônico Prata').setLabel('Prata'),
+				new StringSelectMenuOptionBuilder().setValue('verde').setDescription('Ancestral Dracônico Verde').setLabel('Verde'),
+				new StringSelectMenuOptionBuilder().setValue('vermelho').setDescription('Ancestral Dracônico Vermelho').setLabel('Vermelho'),
+			];
+		},
+		gnomo: () => {
+			return [
+				new StringSelectMenuOptionBuilder().setValue('gnomo da floresta').setDescription('raça Gnomo da Floresta').setLabel('Gnomo da Floresta'),
+				new StringSelectMenuOptionBuilder().setValue('gnomo das rochas').setDescription('raça Gnomo das Rochas').setLabel('Gnomo das Rochas'),
+			];
+		},
+		meioElfo: () => {
+			return new StringSelectMenuOptionBuilder().setValue('meioElfo').setDescription('raça Meio-Elfo').setLabel('Meio-Elfo');
+		},
+		meioOrc: () => {
+			return new StringSelectMenuOptionBuilder().setValue('meioOrc').setDescription('raça Meio-Orc').setLabel('Meio-Orc');
+		},
+		tiefling: () => {
+			return new StringSelectMenuOptionBuilder().setValue('tiefling').setDescription('raça Tiefling').setLabel('Tiefling');
+		},
+	};
+	const getRaces = avalibleRace[race];
+	return getRaces();
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('criar-personagem')
-		.setDescription('Comando para criar o personagem no canal especifico usando o "/criar"'),
+		.setDescription('Comando para criar o personagem no canal especifico usando o "/criar-canal"'),
 
 	/**
      * @param {import('discord.js').Interaction} interaction
@@ -12,7 +80,8 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply();
 
-		const characterSchema = {};
+		const character = {};
+		const RESPONSE_TIME = 10000;
 
 		let validation = false;
 
@@ -21,74 +90,138 @@ module.exports = {
 				member.roles.cache.filter(role => {
 					if (role.name.toLowerCase() === interaction.channel.name) {
 						validation = true;
-						characterSchema.id = role.id;
-						characterSchema.name = role.name;
+						character.id = role.id;
+						character.name = role.name;
 					}
 				});
 			});
 
 		if (!validation) {
-			await interaction.editReply('Você precisa executar o comando /criar com o nome do personagem antes ' + getRandomEmoji());
+			interaction.editReply('Você precisa executar o comando /criar com o nome do personagem antes ' + getRandomEmoji());
 		}
 
 		else {
-			try {
-				let select = new StringSelectMenuBuilder()
-					.setPlaceholder('Selecione a sua classe')
-					.setCustomId('class')
-					.addOptions(
-						new StringSelectMenuOptionBuilder().setValue('guerreiro').setLabel('Guerreiro').setDescription('O Guerreiro combina uma alta força e destreza'),
-						new StringSelectMenuOptionBuilder().setValue('cavaleiro').setLabel('Cavaleiro').setDescription('O Cavaleiro começa com maior vitalidade'),
-						new StringSelectMenuOptionBuilder().setValue('andarilho').setLabel('Andarilho').setDescription('O Andarilho é focado em velocidade e evasão'),
-						new StringSelectMenuOptionBuilder().setValue('ladrao').setLabel('Ladrão').setDescription('Os Ladrões tem alta chance de critico com adaga'),
-						new StringSelectMenuOptionBuilder().setValue('bandido').setLabel('Bandido').setDescription('Os Bandidos são especializados em ataques fisicos fortes'),
-						new StringSelectMenuOptionBuilder().setValue('cacador').setLabel('Caçador').setDescription('Os Caçadores são especialistas em arcos'),
-						new StringSelectMenuOptionBuilder().setValue('feiticeiro').setLabel('Feiticeiro').setDescription('Os Feiticeiros são especialistas em magias'),
-						new StringSelectMenuOptionBuilder().setValue('piromante').setLabel('Piromante').setDescription('Os Piromantes são especialistas em pirotecnias'),
-						new StringSelectMenuOptionBuilder().setValue('clerigo').setLabel('Clérigo').setDescription('Os Clérigos usam os milagres a seu favor'),
-						new StringSelectMenuOptionBuilder().setValue('depravado').setLabel('Depravado').setDescription('Os Depravados não possuem nada ao seu auxilio'),
-					);
+			await interaction.editReply(`O seu personagem se chama ${bold(character.name)}`);
 
-				let row = new ActionRowBuilder().setComponents(select);
+			const filter = m => m.author.id === interaction.user.id;
 
-				const characterRole = await interaction.followUp({ content: 'Qual será a sua classe?', components: [row] });
+			await interaction.followUp({
+				content: 'Qual a idade do personagem?',
+				withResponse: true,
+			})
+				.then(async message => {
+					await message.channel.awaitMessages({ filter: filter, max: 1, time: RESPONSE_TIME, errors: ['time'] })
+						.then(async collected => {
+							character.age = collected.first().content;
+							await message.delete();
+							await collected.first().delete();
+						})
+						.catch(() => {
+							interaction.channel.send('Não foi respondida a pergunta');
+						});
+				});
 
-				const componentfilter = i => i.user.id === interaction.user.id;
+			const baseRace = new StringSelectMenuBuilder()
+				.setPlaceholder('Selecione sua raça')
+				.setCustomId('baseRace')
+				.addOptions([
+					new StringSelectMenuOptionBuilder().setValue('anao').setDescription('raça Anão').setLabel('Anão'),
+					new StringSelectMenuOptionBuilder().setValue('elfo').setDescription('raça Elfo').setLabel('Elfo'),
+					new StringSelectMenuOptionBuilder().setValue('halfling').setDescription('raça Halfling').setLabel('Halfling'),
+					new StringSelectMenuOptionBuilder().setValue('humano').setDescription('raça Humano').setLabel('Humano'),
+					new StringSelectMenuOptionBuilder().setValue('draconato').setDescription('raça Draconato').setLabel('Draconato'),
+					new StringSelectMenuOptionBuilder().setValue('gnomo').setDescription('raça Gnomo').setLabel('Gnomo'),
+					new StringSelectMenuOptionBuilder().setValue('meioElfo').setDescription('raça Meio-Elfo').setLabel('Meio-elfo'),
+					new StringSelectMenuOptionBuilder().setValue('meioOrc').setDescription('raça Meio-Orc').setLabel('Meio-Orc'),
+					new StringSelectMenuOptionBuilder().setValue('tiefling').setDescription('raça Tiefling').setLabel('Tiefling'),
 
-				const roleResponse = await characterRole.awaitMessageComponent({ filter: componentfilter, time: 10000 });
-				await roleResponse.update({ content: `Você escolheu ${roleResponse.values} como classe`, components: [] });
+				]);
 
-				select = new StringSelectMenuBuilder()
-					.setPlaceholder('Selecione a sua classe')
-					.setCustomId('initial')
-					.addOptions(
-						new StringSelectMenuOptionBuilder().setValue('binoculos').setLabel('Binoculos').setDescription('Um Binoculos'),
-						new StringSelectMenuOptionBuilder().setValue('bomba de fogo negra').setLabel('Bomba de fogo Negra').setDescription('Uma bomba negra que explode no impacto'),
-						new StringSelectMenuOptionBuilder().setValue('bencao divina').setLabel('Benção Divina').setDescription('Uma água com a benção das divindades'),
-						new StringSelectMenuOptionBuilder().setValue('chave mestra').setLabel('Chave Mestra').setDescription('Um molho de chaves'),
-						new StringSelectMenuOptionBuilder().setValue('anel antigo da bruxa').setLabel('Anel antigo da Bruxa').setDescription('Um anel antigo, empoeirado e enferrujado'),
-						new StringSelectMenuOptionBuilder().setValue('pingente').setLabel('Pingente').setDescription('Um pingente com uma figura desenhada'),
-						new StringSelectMenuOptionBuilder().setValue('anel pequenino').setLabel('Anel Pequenino').setDescription('Um anel pequeno com uma joia vermelha'),
-						new StringSelectMenuOptionBuilder().setValue('humanidade gemea').setLabel('Humanidade Gemea').setDescription('Duas humanidades interligadas'),
-					);
+			const baseRaceRow = new ActionRowBuilder().addComponents(baseRace);
 
-				row = new ActionRowBuilder().setComponents(select);
+			let selectedBaseRace = '';
 
-				const initialItem = await interaction.followUp({ content: 'Qual será seu item inicial?', components: [row] });
+			const componentfilter = i => i.user.id === interaction.user.id;
 
-				const initialItemResponse = await initialItem.awaitMessageComponent({ filter: componentfilter, time: 10000 });
-				await initialItemResponse.update({ content: `Você escolheu ${initialItemResponse.values} como item inicial`, components: [] });
+			await interaction.followUp({
+				content: 'Qual sua raça base?',
+				withResponse: true,
+				components: [baseRaceRow],
+			})
+				.then(async message => {
+					await message.awaitMessageComponent({ filter: componentfilter, time: RESPONSE_TIME, errors: ['time'] })
+						.then(async collected => {
+							selectedBaseRace = collected.values.toString();
+							character.baseRace = selectedBaseRace;
+							await message.delete();
+						})
+						.catch(() => {
+							interaction.channel.send('erro na resposta');
+						});
+				});
 
-				characterSchema.role = roleResponse.values[0];
-				characterSchema.initial = initialItemResponse.values[0];
+			const subRace = new StringSelectMenuBuilder()
+				.setPlaceholder('Selecione sua sub-raça')
+				.setCustomId('subRace')
+				.addOptions(subRaceSelector(selectedBaseRace));
 
-				axios.post('personagem/criar', characterSchema)
-					.then(res => interaction.followUp(res.data.message));
-			}
-			catch (error) {
-				interaction.followUp(error.message);
-			}
+			const subRaceRow = new ActionRowBuilder().addComponents(subRace);
+
+			await interaction.followUp({
+				content: 'Qual sua sub raça?',
+				withResponse: true,
+				components: [subRaceRow],
+			})
+				.then(async message => {
+					await message.awaitMessageComponent({ filter: componentfilter, time: RESPONSE_TIME, errors:['time'] })
+						.then(async collected => {
+							character.subRace = collected.values.toString();
+							await message.delete();
+						})
+						.catch(() => {
+							interaction.channel.send('erro na resposta');
+						});
+				});
+
+			const role = new StringSelectMenuBuilder()
+				.setPlaceholder('Selecione a sua classe')
+				.setCustomId('role')
+				.addOptions([
+					new StringSelectMenuOptionBuilder().setValue('guerreiro').setLabel('Guerreiro').setDescription('O Guerreiro combina uma alta força e destreza'),
+					new StringSelectMenuOptionBuilder().setValue('cavaleiro').setLabel('Cavaleiro').setDescription('O Cavaleiro começa com maior vitalidade'),
+					new StringSelectMenuOptionBuilder().setValue('andarilho').setLabel('Andarilho').setDescription('O Andarilho é focado em velocidade e evasão'),
+					new StringSelectMenuOptionBuilder().setValue('ladrao').setLabel('Ladrão').setDescription('Os Ladrões tem alta chance de critico com adaga'),
+					new StringSelectMenuOptionBuilder().setValue('bandido').setLabel('Bandido').setDescription('Os Bandidos são especializados em ataques fisicos fortes'),
+					new StringSelectMenuOptionBuilder().setValue('cacador').setLabel('Caçador').setDescription('Os Caçadores são especialistas em arcos'),
+					new StringSelectMenuOptionBuilder().setValue('feiticeiro').setLabel('Feiticeiro').setDescription('Os Feiticeiros são especialistas em magias'),
+					new StringSelectMenuOptionBuilder().setValue('piromante').setLabel('Piromante').setDescription('Os Piromantes são especialistas em pirotecnias'),
+					new StringSelectMenuOptionBuilder().setValue('clerigo').setLabel('Clérigo').setDescription('Os Clérigos usam os milagres a seu favor'),
+					new StringSelectMenuOptionBuilder().setValue('depravado').setLabel('Depravado').setDescription('Os Depravados não possuem nada ao seu auxilio'),
+				]);
+
+			const roleRow = new ActionRowBuilder().addComponents(role);
+
+			await interaction.followUp({
+				content: 'Qual a sua classe?',
+				withResponse: true,
+				components: [roleRow],
+			})
+				.then(async message => {
+					await message.awaitMessageComponent({ filter: componentfilter, time: RESPONSE_TIME, errors: ['time'] })
+						.then(async collected => {
+							character.role = collected.values.toString();
+							await message.delete();
+						});
+				});
+
+			console.log(character);
+
+			const { data } = await axios.post(
+				'/personagem/criar',
+				character,
+			);
+
+			interaction.channel.send(data.message);
 		};
-
 	},
 };
