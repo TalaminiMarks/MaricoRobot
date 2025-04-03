2f28d317282c01c1782c207cfce2ee938fea7eeb
TalaminiMarks <talamini2014@gmail.com>
2025-04-02 22:16:57 -0300

    move subRaces data to its own file

---
+++ data/race.js
@@ -0,0 +1,52 @@
+module.exports = {
+	// races data
+	dwarf: [
+		{ value: 'anao da montanha', desctiption: 'Raça Anão da Montanha' },
+		{ value: 'anao da colina', desctiption: 'Raça Anão da Colina' },
+	],
+	elf: [
+		{ value: 'alto elfo', desctiption: 'Raça Alto Elfo' },
+		{ value: 'elfo da floresta', desctiption: 'Raça Elfo da Floresta' },
+		{ value: 'elfo negro', desctiption: 'Raça Elfo Negro' },
+	],
+	hafling: [
+		{ value: 'halfling pes leves', desctiption: 'Raça Halfling Pes Leves' },
+		{ value: 'halfling robusto', desctiption: 'Raça Halfling Robusto' },
+	],
+	human: [
+		{ value: 'calishita', desctiption: 'Humano Calishita' },
+		{ value: 'chondathano', desctiption: 'Humano Chondathano' },
+		{ value: 'damarano', desctiption: 'Humano Damarano' },
+		{ value: 'illuskano', desctiption: 'Humano Illuskano' },
+		{ value: 'mulano', desctiption: 'Humano Mulano' },
+		{ value: 'rashemita', desctiption: 'Humano Rashemita' },
+		{ value: 'shou', desctiption: 'Humano Shou' },
+		{ value: 'tethyriano', desctiption: 'Humano Tethyriano' },
+		{ value: 'turami', desctiption: 'Humano Turami' },
+	],
+	dragonborn: [
+		{ value: 'azul', desctiption: 'Ancestral Dracônico Azul' },
+		{ value: 'branco', desctiption: 'Ancestral Dracônico Branco' },
+		{ value: 'bronze', desctiption: 'Ancestral Dracônico Bronze' },
+		{ value: 'cobre', desctiption: 'Ancestral Dracônico Cobre' },
+		{ value: 'latao', desctiption: 'Ancestral Dracônico Latão' },
+		{ value: 'negro', desctiption: 'Ancestral Dracônico Negro' },
+		{ value: 'ouro', desctiption: 'Ancestral Dracônico Ouro' },
+		{ value: 'prata', desctiption: 'Ancestral Dracônico Prata' },
+		{ value: 'verde', desctiption: 'Ancestral Dracônico Verde' },
+		{ value: 'vermelho', desctiption: 'Ancestral Dracônico Vermelho' },
+	],
+	gnomo: [
+		{ value: 'gnomo da floresta', desctiption: 'Raça Gnomo da Floresta' },
+		{ value: 'gnomo das rochas', desctiption: 'Raça Gnomo das Rochas' },
+	],
+	halfElf: [
+		{ value: 'meio elfo', desctiption: 'Raça Meio-Elfo' },
+	],
+	halfOrc: [
+		{ value: 'meio orc', desctiption: 'Raça Meio-Orc' },
+	],
+	tiefling: [
+		{ value: 'tiefling', desctiption: 'Raça Tiefling' },
+	],
+};
