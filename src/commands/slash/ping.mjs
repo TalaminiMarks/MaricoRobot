692e2cccfa8bdbc56694629b2848e362477a2068
TalaminiMarks <talamini2014@gmail.com>
2025-04-10 16:03:53 -0300

    switch from commonJS to modules

---
+++ src/commands/slash/ping.mjs
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+	.setName('ping')
+	.setDescription('Responde com Pong!');
+
+/**
+* @param {import('discord.js').Interaction} interaction
+*/
+export async function execute(interaction) {
+	await interaction.reply('Pong');
+}
